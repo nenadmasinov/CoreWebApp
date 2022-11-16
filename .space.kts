69a635812bcf4b40577dcb2ab8b82ef80e73cecf
Nenad Blafsoft <nenad@blafsoft.com>
2022-11-16 14:41:14 +0000

    updating build and publish job script

--- .space.kts
+++ .space.kts
@@ -4,6 +4,17 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job(".NET Core desktop. Build, publish"){
+    container(image = "mcr.microsoft.com/dotnet/core/sdk:3.1"){
+        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/mycompany/p/pkey/mynuget/v3/index.json"
+        shellScript {
+            content = """
+                echo Run build...
+                dotnet build
+                echo Publish NuGet package...
+                chmod +x publish.sh
+                ./publish.sh
+            """
+        }
+    }
 }
