8ad5c5733603a8e6e81d85c8903504d1846adca4
Nenad Blafsoft <nenad@blafsoft.com>
2022-11-16 15:22:04 +0000

    changing dotnet container from 3.1 to 6.0

--- .space.kts
+++ .space.kts
@@ -5,7 +5,7 @@
 */
 
 job(".NET Core desktop. Build, publish"){
-    container(image = "mcr.microsoft.com/dotnet/core/sdk:3.1"){
+    container(image = "mcr.microsoft.com/dotnet/core/sdk:6.0"){
         env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/mycompany/p/pkey/mynuget/v3/index.json"
         shellScript {
             content = """
